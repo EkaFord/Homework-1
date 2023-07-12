202622c2824e58fbf3a8e80d6324397f65bd2d52
EkaFord <138514574+EkaFord@users.noreply.github.com>
2023-07-11 23:03:12 -0400

    fisrt js changes

---
+++ hw2.js
@@ -0,0 +1,51 @@
+let a = 10;
+alert(a); // put 10
+a = 20;
+alert(a); // put 20
+
+/*2*/
+a = 2007;
+alert(a); // put 2007
+
+/*3*/
+a = 'Brendan Eich'
+alert(a); // put Brendan Eich
+
+/*4*/
+let b = 10;
+let c = 2;
+
+alert("Сумма: " + (b + c));
+alert("Разность: " + (b - c));
+alert("Произведение: " + (b * c));
+alert("Частное: " + (b / c));
+
+/*5*/
+let result = Math.pow(2, 5);
+alert(result);
+
+/*6*/
+let d = 9;
+let e = 2
+alert(d % e); //остаток от деления
+
+/*7*/
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num + 1;
+num = num - 1;
+alert(num);
+
+/*8*/
+let age = prompt("Сколько вам лет?");
+alert("Ваш возраст: " + age);
+
+/*9*/
+let user = {
+    name: "Melissa",
+    age: 25,
+    isAdmin: true
+  };
