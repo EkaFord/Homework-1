388b0d112a850a482e3d5fad4e561bbad2ed9644
EkaFord <138514574+EkaFord@users.noreply.github.com>
2023-08-08 22:38:22 -0400

    2

---
+++ exam.js
@@ -0,0 +1,12 @@
+let age = promt("How old are you?");
+
+let message = age >= 18 ? "Adult" : "Not adult";
+
+console.log(message);
+
+function showAd() {
+    alert("Today only game 2 50%");
+}
+
+setTimeout(showAd, 1000 * 60);
+ 
